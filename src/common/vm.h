cdd02536a8ae531f106c3c9d358c34f1da608c65
secretval <108518296+SecretVal@users.noreply.github.com>
2025-03-27 16:27:16 +0100

    basic

--- src/common/vm.h
+++ src/common/vm.h
@@ -3,29 +3,29 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define JE 0
-#define JGE 1
-#define JG 2
-#define JLE 3
-#define JL 3
-
 typedef enum {
-  OP_NOP = 0x0,
-  OP_PUSH,
-  OP_POP,
-  OP_LOAD_REG,
-  OP_PUSH_REG,
-  OP_CMP_REG,
-  OP_CMP_STACK,
-  OP_JMP,
-  OP_JGE,
-  OP_JG,
-  OP_JLE,
-  OP_JL,
+  OP_NOP = 0x0, // 0
+  OP_PUSH,      // 1
+  OP_POP,       // 2
+  OP_LOAD_REG,  // 3
+  OP_PUSH_REG,  // 4
+  OP_ADD_STACK, // 5
+  OP_ADD_REG,   // 6
+  OP_CMP_REG,   // 7
+  OP_CMP_STACK, // 8
+  OP_JMP,       // 9
+  OP_JE,        // 10
+  OP_JGE,       // 11
+  OP_JG,        // 12
+  OP_JLE,       // 13
+  OP_JL,        // 14
 } OpCode;
 
 #define REG_COUNT 26
-typedef enum { A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z } Register;
+typedef enum { REG_A,REG_B,REG_C,REG_D,REG_E,REG_F,REG_G,REG_H,REG_I,REG_J,REG_K,REG_L,REG_M,REG_N,REG_O,REG_P,REG_Q,REG_R,REG_S,REG_T,REG_U,REG_V,REG_W,REG_X,REG_Y,REG_Z } Register;
+
+#define FLAG_COUNT 2
+typedef enum  { FLAG_CF, FLAG_ZF } Flag;
 
 typedef struct {
   uint64_t *items;
--- src/vic/vic.c
+++ src/vic/vic.c
@@ -12,6 +12,7 @@ typedef struct {
   Program program;
   uint64_t stack[STACK_SIZE];
   uint64_t regs[REG_COUNT];
+  uint64_t flags[FLAG_COUNT];
 
   size_t sp;
   size_t pc;
@@ -25,7 +26,8 @@ bool vm_push(VM *vm, uint64_t val) {
   return true;
 }
 
-void vm_pop(VM *vm, Register reg) {
+uint64_t vm_pop(VM *vm, Register reg) {
+  uint64_t res =   vm->stack[vm->sp];
   if (vm->sp == 0) {
     vm->regs[reg] = vm->stack[vm->sp];
     vm->stack[vm->sp] = 0;
@@ -35,6 +37,7 @@ void vm_pop(VM *vm, Register reg) {
     vm->stack[vm->sp] = 0;
     vm->sp--;
   }
+  return res;
 }
 
 inline void vm_load_reg(VM *vm, Register reg, uint64_t val) {
@@ -46,35 +49,57 @@ bool vm_push_reg(VM *vm, Register reg)  {
 }
 
 // TODO: COMPARING
-/*static uint64_t compare(uint64_t a, uint64_t b) {*/
-/*  todo("error");*/
-/*  if (a == b) return JE;*/
-/*  if (a >= b) return JGE;*/
-/*  if (a > b) return JG;*/
-/*  if (a <= b) return JLE;*/
-/*  if (a < b) return JL;*/
-/*  fprintf(stderr, "UNREACHABLE");*/
-/*  abort();*/
-/*}*/
-/**/
-/*bool vm_cmp_reg(VM *vm, Register ra, Register rb) {*/
-/*  uint64_t a = vm->regs[a];*/
-/*  uint64_t b = vm->regs[b];*/
-/*  return vm_push(vm, compare(a, b));*/
-/*}*/
-/**/
-/*bool vm_cmp_stack(VM *vm) {*/
-/*  uint64_t a = vm->stack[vm->sp];*/
-/*  uint64_t b = vm->stack[vm->sp - 1];*/
-/*  return vm_push(vm, compare(a, b));*/
-/*}*/
-/**/
-/*bool vm_jmp(VM *vm, uint64_t dest) {*/
-/*  if (dest > vm->program.count || dest < 0)*/
-/*    return false;*/
-/*  vm->pc = dest;*/
-/*  return true;*/
-/*}*/
+static inline void compare(VM *vm, uint64_t a, uint64_t b) {
+  uint64_t res = a - b;
+  if (res == 0) vm->flags[FLAG_ZF] = 0;
+  if (res < 0) vm->flags[FLAG_CF] = 0;
+  if (res > 0) vm->flags[FLAG_CF] = 1;
+}
+
+static inline void vm_cmp_reg(VM *vm, Register ra, Register rb) {
+  uint64_t a = vm->regs[ra];
+  uint64_t b = vm->regs[rb];
+  compare(vm, a, b);
+}
+
+static inline void vm_cmp_stack(VM *vm) {
+  uint64_t a = vm->stack[vm->sp];
+  uint64_t b = vm->stack[vm->sp - 1];
+  compare(vm, a, b);
+}
+
+bool vm_jmp(VM *vm, uint64_t dest) {
+  if (dest > vm->program.count || dest < 0)
+    return false;
+  vm->pc = dest;
+  return true;
+}
+
+bool vm_je(VM *vm, uint64_t dest) {
+  if (vm->flags[FLAG_ZF] == 0)
+    return vm_jmp(vm, dest);
+  return true;
+}
+
+bool vm_jg(VM *vm, uint64_t dest) {
+  if (vm->flags[FLAG_CF])
+    return vm_jmp(vm, dest);
+  return false;
+}
+
+bool vm_jge(VM *vm, uint64_t dest) {
+  return vm_je(vm, dest) || vm_jg(vm, dest);
+}
+
+bool vm_jl(VM *vm, uint64_t dest) {
+  if (!vm->flags[FLAG_CF])
+    return vm_jmp(vm, dest);
+  return false;
+}
+
+bool vm_jle(VM *vm, uint64_t dest) {
+  return vm_je(vm, dest) || vm_jl(vm, dest);
+}
 
 inline static uint64_t vm_read(VM *vm) {
   uint64_t inst = vm->program.items[vm->pc];
@@ -84,55 +109,119 @@ inline static uint64_t vm_read(VM *vm) {
 
 void vm_next(VM *vm) {
   uint64_t inst = vm_read(vm);
-  uint64_t val;
-  uint64_t reg;
+  /*println("%zu => %u", vm->pc-1, (unsigned int)inst);*/
   switch (inst) {
-    case OP_NOP:
-      break;
-    case OP_PUSH:
-      val = vm_read(vm);
+    case OP_NOP: break;
+    case OP_PUSH: {
+      uint64_t val = vm_read(vm);
       if (!vm_push(vm, val)){
         fprintln(stderr, "stack overflow");
         exit(1);
       }
       break;
-    case OP_POP:
-      reg = vm_read(vm);
-      assert(reg >= A && val <= Z);
+    }
+    case OP_POP: {
+      uint64_t reg = vm_read(vm);
+      assert(reg >= REG_A && reg <= REG_Z);
       vm_pop(vm, reg);
       break;
-    case OP_LOAD_REG:
-      reg = vm_read(vm);
-      val = vm_read(vm);
+    }
+    case OP_ADD_REG: {
+      Register dest = vm_read(vm);
+      Register src = vm_read(vm);
+      assert(dest >= REG_A && dest <= REG_Z);
+      assert(src >= REG_A && src <= REG_Z);
+      vm->regs[dest] = vm->regs[dest] + vm->regs[src];
+      break;
+    }
+    case OP_ADD_STACK: {
+      assert(vm->sp > 0);
+      uint64_t a = vm->stack[vm->sp];
+      uint64_t b = vm->stack[vm->sp - 1];
+      vm_push(vm, a + b);
+      break;
+    }
+    case OP_LOAD_REG: {
+      uint64_t reg = vm_read(vm);
+      uint64_t val = vm_read(vm);
       vm_load_reg(vm, reg, val);
       break;
-    case OP_PUSH_REG:
-      reg = vm_read(vm);
+    }
+    case OP_PUSH_REG: {
+      uint64_t reg = vm_read(vm);
       vm_push_reg(vm, reg);
       break;
-    default:
-      todo("vm_next invalid opcode");
+    }
+    case OP_CMP_REG: {
+      uint64_t a = vm_read(vm);
+      uint64_t b = vm_read(vm);
+      assert(a >= REG_A && a <= REG_Z);
+      assert(b >= REG_A && b <= REG_Z);
+      vm_cmp_reg(vm, a, b);
+      break;
+    }
+    case OP_CMP_STACK:
+      vm_cmp_stack(vm);
+      break;
+    case OP_JMP: {
+      uint64_t dest = vm_read(vm);
+      if (!vm_jmp(vm, dest)) {
+        fprintln(stderr, "invalid jump dest");
+        exit(1);
+      }
+      break;
+    }
+    case OP_JE: {
+      uint64_t dest = vm_read(vm);
+      if (!vm_je(vm, dest)) {
+        fprintln(stderr, "invalid jump dest");
+        exit(1);
+      }
+      break;
+    }
+    default: todo(temp_sprintf("vm_next invalid opcode %u %zu", (unsigned int)inst, vm->pc));
   }
 }
 
 int main(int argc, char *argv[]) {
   VM vm = {.sp = -1};
+  // REALLY IMPORTANT
+  vm.flags[FLAG_ZF] = -1;
+  vm.flags[FLAG_CF] = -1;
 
   da_push(&vm.program, OP_LOAD_REG);
-  da_push(&vm.program, A);
-  da_push(&vm.program, 0xFF);
+  da_push(&vm.program, REG_A);
+  da_push(&vm.program, 0x1);
 
-  da_push(&vm.program, OP_PUSH_REG);
-  da_push(&vm.program, A);
+  da_push(&vm.program, OP_LOAD_REG);
+  da_push(&vm.program, REG_B);
+  da_push(&vm.program, 0x1);
 
-  da_push(&vm.program, OP_POP);
-  da_push(&vm.program, B);
+  da_push(&vm.program, OP_LOAD_REG);
+  da_push(&vm.program, REG_Z);
+  da_push(&vm.program, 0xFFF);
 
+  da_push(&vm.program, OP_ADD_REG);
+  da_push(&vm.program, REG_A);
+  da_push(&vm.program, REG_B);
+
+  da_push(&vm.program, OP_CMP_REG);
+  da_push(&vm.program, REG_A);
+  da_push(&vm.program, REG_Z);
+
+  // for ASM: jne could just be this (NOTE assembler would need to track the count of instructions)
+  da_push(&vm.program, OP_JE);
+  da_push(&vm.program, vm.program.count + 3);
+
+  da_push(&vm.program, OP_JMP);
+  da_push(&vm.program, 9);
+
+  da_push(&vm.program, OP_NOP);
 
   while (vm.pc <= vm.program.count)
     vm_next(&vm);
 
-  println("A %u", vm.regs[A]);
-  println("B %u", vm.regs[B]);
+  println("Did %u iterations", (unsigned int)vm.regs[REG_A]);
+
   return 0;
 }
