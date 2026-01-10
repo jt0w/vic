09dca08853724ed38881e2ffcfa5dc5d9331312d
secretval <108518296+SecretVal@users.noreply.github.com>
2026-01-10 15:44:49 +0100

    format "INST" macros

--- src/common/vm.h
+++ src/common/vm.h
@@ -28,18 +28,18 @@ typedef struct {
   uint64_t operand;
 } Inst;
 
-#define INST_NOP ((Inst){.opcode = OP_NOP})
-#define INST_PUSH(x) ((Inst){.opcode = OP_PUSH, .operand = (x)})
-#define INST_POP ((Inst){.opcode = OP_POP})
-#define INST_DUP(x) ((Inst){.opcode = OP_DUP, .operand = (x)})
-#define INST_ADD ((Inst){.opcode = OP_ADD})
-#define INST_SUB ((Inst){.opcode = OP_SUB})
-#define INST_MULT ((Inst){.opcode = OP_MULT})
-#define INST_DIV ((Inst){.opcode = OP_DIV})
-#define INST_EQ ((Inst){.opcode = OP_EQ})
-#define INST_JMP(x) ((Inst){.opcode = OP_JMP, .operand = (x)})
-#define INST_JNZ(x) ((Inst){.opcode = OP_JNZ, .operand = (x)})
-#define INST_JZ(x) ((Inst){.opcode = OP_JZ, .operand = (x)})
+#define INST_NOP     ((Inst){.opcode = OP_NOP                })
+#define INST_PUSH(x) ((Inst){.opcode = OP_PUSH,.operand = (x)})
+#define INST_POP     ((Inst){.opcode = OP_POP                })
+#define INST_DUP(x)  ((Inst){.opcode = OP_DUP, .operand = (x)})
+#define INST_ADD     ((Inst){.opcode = OP_ADD                })
+#define INST_SUB     ((Inst){.opcode = OP_SUB                })
+#define INST_MULT    ((Inst){.opcode = OP_MULT               })
+#define INST_DIV     ((Inst){.opcode = OP_DIV                })
+#define INST_EQ      ((Inst){.opcode = OP_EQ                 })
+#define INST_JMP(x)  ((Inst){.opcode = OP_JMP, .operand = (x)})
+#define INST_JNZ(x)  ((Inst){.opcode = OP_JNZ, .operand = (x)})
+#define INST_JZ(x)   ((Inst){.opcode = OP_JZ,  .operand = (x)})
 
 typedef struct {
   Inst *items;
